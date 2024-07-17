ca167bfefb28d53bc550a86f83c75649632c3572
jiwonniddaaa <jiwonniddaaa02@gmail.com>
2024-07-16 17:51:40 -0700

    login page success

--- src/main/java/GreenishApplication.java
+++ src/main/java/GreenishApplication.java
@@ -1,8 +1,9 @@
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @ComponentScan(basePackages = "com.solux.greenish")
 public class GreenishApplication {
 
